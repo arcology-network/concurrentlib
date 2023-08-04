45bbed922799c183430d47a84f2fe1450684169b
laurentyzhang <laurent.y.zhang@gmail.com>
2023-08-04 15:47:36 -0600

    Fixed a unit test

--- examples/vote/VoteParallelizedMp.sol
+++ examples/vote/VoteParallelizedMp.sol
@@ -65,10 +65,10 @@ contract ParaBallot {
         //     msg.sender == chairperson,
         //     "Only chairperson can give right to vote."
         // );
-        // require(
-        //     !voters[voter].voted,
-        //     "The voter already voted."
-        // );
+        require(
+            !voters[voter].voted,
+            "The voter already voted."
+        );
         require(voters[voter].weight == 0);
         voters[voter].weight = 1;
     }
