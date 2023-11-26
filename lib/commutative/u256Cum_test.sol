e6f8b11ae427c993ab3062fd05348901114ce50f
Laurent Zhang <laurentz@arcology.network>
2023-11-26 00:39:06 -0700

    More unit test

--- lib/commutative/u256Cum_test.sol
+++ lib/commutative/u256Cum_test.sol
@@ -39,5 +39,28 @@ contract CumulativeU256Test {
         require(cumulative.min() == 0);
         require(cumulative.max() == 100);
     }
+
+    function call() public {
+        cumulative.add(1);
+        require(cumulative.get() == 11);
+    }
 }
 
+contract VisitCounter {    
+    U256Cumulative visitCount;
+    event CounterQuery(uint256 value);
+
+    constructor()  {
+        visitCount = new U256Cumulative(0, 1000000);
+    }
+
+    function call() public {
+        visitCount.add(1);
+        require(visitCount.get() == 1);
+    }
+
+    function getCounter() public returns(uint256){
+        emit CounterQuery(visitCount.get());
+        return visitCount.get();
+    }
+}
