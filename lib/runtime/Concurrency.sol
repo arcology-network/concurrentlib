75718b85c9182a0f71e5945ca1482b3cd9810dad
Laurent Zhang <laurentz@arcology.network>
2024-02-18 10:02:53 -0700

    Refactor Concurrency and Runtime contracts

--- lib/runtime/Concurrency.sol
+++ lib/runtime/Concurrency.sol
@@ -1,37 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.8.0 < 0.9.0;
+// pragma solidity >= 0.8.0 < 0.9.0;
 
-/**
- * @author Arcology Network
- * @title Runtime Contract
- * @dev The Runtime contract provides runtime information to developers.
- *      It exposes functions to retrieve pseudo-process IDs (PIDs) and pseudo-random UUIDs.
- */
-library Concurrency {
-    /**
-     * @notice Get the pseudo-process ID (PID) from an external contract.
-     * @dev The PID is a pseudo-process ID and does not represent a real transaction being processed.
-     * @return args The pseudo-process ID (PID) returned by the external contract.
-     */
-    function max(string memory func, uint256 num) public returns(bytes memory) {
-        (,bytes memory random) = address(0xd0).call(abi.encodeWithSignature("pid()"));     
-        return random;
-    }
+// /**
+//  * @author Arcology Network
+//  * @title Runtime Contract
+//  * @dev The Runtime contract provides runtime information to developers.
+//  *      It exposes functions to retrieve pseudo-process IDs (PIDs) and pseudo-random UUIDs.
+//  */
+// library Concurrency {
+//     /**
+//      * @notice Get the pseudo-process ID (PID) from an external contract.
+//      * @dev The PID is a pseudo-process ID and does not represent a real transaction being processed.
+//      * @return args The pseudo-process ID (PID) returned by the external contract.
+//      */
+//     function max(string memory func, uint256 num) public returns(bytes memory) {
+//         (,bytes memory random) = address(0xd0).call(abi.encodeWithSignature("pid()"));     
+//         return random;
+//     }
 
-    /**
-     * @notice Get execution scheduler information for the block.
-     */
-    function conflictWith(string[] memory funcs) public {
-        address(0xd0).call(abi.encodeWithSignature("schedule()"));
-    }
+//     /**
+//      * @notice The funtion instructs the scheduler to avoid executing the specified functions with itself in parallel.
+//      * @param funcs The list of function signatures and their contract addresses to avoid executing in parallel.
+//      */
+//     function avoid(string[] memory funcs) public {
+//         address(0xd0).call(abi.encodeWithSignature("avoid()"));
+//     }
 
-    /**
-     * @notice Get execution scheduler information for the block.
-     * @return The scheduler information for the block.
-     */
-    function schedule() public returns(uint256, uint256, uint256, uint256) {
-        (,bytes memory data) = address(0xd0).call(abi.encodeWithSignature("schedule()"));
-        return abi.decode(data, (uint256, uint256, uint256, uint256));  
-    }
-}
+//     /**
+//      * @notice Get the number of concurrent instances of the specified function.
+//      * @return The scheduler information for the block.
+//      */
+//     function instances(address addr, string memory func) public returns(uint256) {
+//         (,bytes memory data) = address(0xd0).call(abi.encodeWithSignature("schedule(address, string)", addr, func));
+//         return abi.decode(data, (uint256));  
+//     }
+// }
  
